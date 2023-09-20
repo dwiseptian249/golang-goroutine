1ca48849024c6e9f4d95e3613c5af21608d5e5eb
Dwi Septian <dwiseptian746@gmail.com>
2023-09-20 21:48:24 +0700

    Golang GOROUTINE

---
+++ channel_test.go
@@ -0,0 +1,151 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateChannnel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Dwi Septian"
+		fmt.Println("Selesai Mengirim Data ke Channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Mesut Ozil"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Ibrahim Ismail"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channnel := make(chan string, 3)
+	defer close(channnel)
+
+	go func() {
+		channnel <- "Nuh"
+		channnel <- "Hud"
+		channnel <- "Sholeh"
+	}()
+
+	go func() {
+		fmt.Println(<-channnel)
+		fmt.Println(<-channnel)
+		fmt.Println(<-channnel)
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 15; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima Data", data)
+	}
+
+	fmt.Println("Done")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
---
+++ gomaxprocs_test.go
@@ -0,0 +1,52 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+	totalCpu := runtime.NumCPU()
+	fmt.Println("Total CPU", totalCpu)
+
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Threads", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine", totalGoroutine)
+
+	group.Wait()
+}
+
+func TestThreadGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+	totalCpu := runtime.NumCPU()
+	fmt.Println("Total CPU", totalCpu)
+
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Threads", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine", totalGoroutine)
+
+	group.Wait()
+}
